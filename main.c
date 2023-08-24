42ab3b30474b9af9b2b388b4d2534baa93aef027
Yebo Kalua <kaluayebo2@gmail.com>
2023-08-24 02:24:36 -0700

    header file

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: command line arguments
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	if (argc == )
+}
---
+++ monty.h
@@ -0,0 +1,10 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/* C standard library */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#endif
