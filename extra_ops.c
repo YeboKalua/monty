d5ba5c690729309f5c99f15465d65de5240b7015
Yebo Kalua <kaluayebo2@gmail.com>
2023-08-26 20:42:52 +0000

    sub

---
+++ extra_ops.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+  * sub - subtracts first two elements
+  * @stack: stack to subtract from
+  * @line_number: number of command
+  */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	stack_t *tmp1, *tmp2;
+	
+	if (args->stack_length < 2)
+	{
+		dprintf(2, "L%d: can't sub, stack too short\n", line_number);
+		total_free();
+		exit(EXIT_FAILURE);
+	}
+	tmp1 = args->head;
+	tmp2 = tmp1->next;
+	tmp2->n = tmp2->n - tmp1->n;
+	delete_node();
+	args->stack_length -= 1;
+}
--- instructions.c
+++ instructions.c
@@ -9,7 +9,8 @@ void get_command(void)
     {"push", &push}, {"pall", &pall},
     {"pint", &pint}, {"pop", &pop},
 	{"swap", &swap}, {"add", &add}, 
-    {"nop", &nop}, {NULL, NULL}
+    {"nop", &nop}, {"sub", &sub} 
+	{NULL, NULL}
 	/*{"stack", &stack},
     {"queue", &queue}, {NULL, NULL}*/
     };
